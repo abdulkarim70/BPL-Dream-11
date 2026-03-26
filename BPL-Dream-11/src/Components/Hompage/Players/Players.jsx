daf753dba69b602a0f8d3623fede541ccd6e82d7
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-03-26 14:13:37 +0600

    working with useState

--- BPL-Dream-11/src/Components/Hompage/Players/Players.jsx
+++ BPL-Dream-11/src/Components/Hompage/Players/Players.jsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from '../../AvailablePlayers/AvailablePlayers';
 
 
@@ -7,9 +7,19 @@ const Players = ({playersPromise}) => {
  console.log(playersPromise);
  const players=use(playersPromise)
   console.log(players);
+  const [selectedType, setselectedType]=useState('available')
     return (
-        <div className='max-w-7xl mx-auto'>
-           players: {players.length}
+        <div className='max-w-7xl mx-auto my-[60px]'>
+          
+<div className='flex justify-between items-center gap-4 mb-[20px]'>
+   <h2 className='font-bold text-3xl'>Available Players</h2> 
+   <div className=''>
+    <button className="btn bg-[#E7FE29] rounded-r-none rounded-l-xl">Available</button>
+    <button className="btn btn-neutral rounded-l-none rounded-r-xl">Selected</button>
+
+   </div>
+</div>
+
            <AvailablePlayers players={players}></AvailablePlayers>
         </div>
     );
